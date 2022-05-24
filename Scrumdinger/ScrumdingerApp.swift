1ee3c58827bcf157cebdbeb0ec82f0d0a60ff6a0
Ayush Pratap Singh <ayushpratap16@gmail.com>
2022-05-25 04:06:10 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ayush Pratap Singh on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
